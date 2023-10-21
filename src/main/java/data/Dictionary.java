6b270801a9319652bbc152386b147ec704f37e51
Nguyễn Văn Sáng <sang080304@gmail.com>
2023-10-21 16:59:16 +0700

    update Dictionary

--- src/main/java/data/Dictionary.java
+++ src/main/java/data/Dictionary.java
@@ -92,4 +92,9 @@ public class Dictionary {
             }
             return null;
       }
+
+      public boolean have(String newString) {
+            int index = getLowerBound(newString);
+            return index >= 0;
+      }
 }
