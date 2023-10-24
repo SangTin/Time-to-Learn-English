66231edee81cbc51a53c0489aad0a3da399bff47
vuhau190904 <vuhau190904@gmail.com>
2023-10-24 23:47:54 +0700

    update

--- src/main/java/entry/CommandLine.java
+++ src/main/java/entry/CommandLine.java
@@ -19,6 +19,7 @@ public class CommandLine {
     }
 
     public void showMenu() {
+        System.out.print("\033\143");
         System.out.print("Welcome to My Application!\n"
                 + "[0] Exit\n"
                 + "[1] Add\n"
@@ -30,58 +31,147 @@ public class CommandLine {
                 + "[7] Game\n"
                 + "[8] Import from file\n"
                 + "[9] Export to file\n"
-                + "Your action:");
+                + "Your action: ");
+
+        int type = sc.nextInt();
+        switch (type) {
+            case 0:
+                break;
+            case 1:
+                Add();
+                break;
+            case 2:
+                Remove();
+                break;
+            case 3:
+                Update();
+                break;
+            case 4:
+                Display();
+                break;
+            case 5:
+                Lookup();
+                break;
+            case 6:
+                Search();
+                break;
+            case 7:
+                game();
+                break;
+            case 8:
+                importFromFile();
+                break;
+            case 9:
+                exportToFile();
+                break;
+            default:
+                break;
+        }
     }
 
     public void Add() {
-        System.out.print("Number of words you want to enter: ");
+        System.out.print("\033\143");
+        System.out.print("Number of words you want to add: ");
         int n = sc.nextInt();
         System.out.println();
+        ArrayList<String> summary = new ArrayList<>();
         for (int i = 1; i <= n; ++i) {
             System.out.print("Word target: ");
             String wordTarget = sc.next();
             System.out.print("Word explain: ");
             String wordExplain = sc.nextLine();
             wordExplain = sc.nextLine();
-            int index = words.getWordTarget().getLowerBound(wordTarget);
-            if (index < 0) {
-                words.insert(new Word(wordTarget, wordExplain, null, null));
+            Word newWord = new Word(wordTarget, wordExplain);
+            if (words.insert(newWord)) {
+                summary.add(wordTarget);
             } else {
                 System.out.print(
-                        "This word was added with another meaning\nDo you want overwrite this word?\nYes or No\nYour choice: ");
+                        "This word was added with another meaning\nDo you want to overwrite this word?\nYes or No\nYour choice: ");
                 String answer = sc.next();
                 answer = answer.toLowerCase();
-                if (answer == "yes")
-                    words.getWords().get(index).setWordExplain(wordExplain);
+                System.out.println(answer);
+                if (answer.equals("yes")) {
+                    words.fix(newWord);
+                }
             }
             System.out.println();
+        }
+
+        System.out.println("Sum up the words you added!");
+        System.out.println();
+        System.out.println("No    |    English    |    Vietnamese");
+        for (int i = 0; i < summary.size(); ++i) {
+            int index = words.getLowerBound(summary.get(i));
+            Word word = words.getWords().get(index);
+            System.out.printf("%-6s", Integer.toString(i + 1));
+            System.out.printf("%-5s", "|");
+            System.out.printf("%-11s", word.getWordTarget());
+            System.out.printf("%-5s", "|");
+            System.out.println(word.getWordExplain());
+        }
+
+
+        System.out.println();
+
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
         }
     }
 
     public void Remove() {
+        System.out.print("\033\143");
         System.out.print("Word you want to remove: ");
         String wordString = sc.next();
-        int index = words.getWordTarget().getLowerBound(wordString);
-        if (index < 0) {
-            System.out.println("There is no that word in the dictionary!");
+        if (!words.remove(wordString)) {
+            System.out.println("This word is not in the dictionary!");
         } else {
-            words.remove(wordString);
             System.out.println("This word is removed!");
         }
         System.out.println();
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
+        }
     }
 
     public void Update() {
+        System.out.print("\033\143");
         System.out.print("Word target: ");
         String wordTarget = sc.next();
         System.out.print("Word explain: ");
         String wordExplain = sc.nextLine();
         wordExplain = sc.nextLine();
         System.out.println();
-        words.fix(new Word(wordTarget, wordExplain, null, null));
+        int index = words.getLowerBound(wordTarget);
+        Word newWord = new Word(wordTarget, wordExplain);
+        Word copy = new Word();
+        if(index >= 0) {
+           copy = words.getWords().get(index);
+        }
+
+        System.out.println();
+        if(words.fix(newWord)) {
+            System.out.println("You changed word explain!");
+            System.out.println(copy.getWordExplain() + " ---------> " + wordExplain);
+        } else {
+            words.insert(newWord);
+            System.out.println("You added 1 word!");
+            System.out.println("Word target: " + wordTarget);
+            System.out.println("Word explain: " + wordExplain);
+        }
+        System.out.println();
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
+        }
     }
 
     public void Display() {
+        System.out.print("\033\143");
         int n = words.getWords().size();
         System.out.println("No    |    English    |    Vietnamese");
         for (int i = 0; i < n; ++i) {
@@ -92,43 +182,75 @@ public class CommandLine {
             System.out.println(words.getWords().get(i).getWordExplain());
         }
         System.out.println();
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
+        }
     }
 
     public void Lookup() {
+        System.out.print("\033\143");
         System.out.print("Word you want to look up: ");
         String wordString = sc.next();
-        int index = words.getWordTarget().getLowerBound(wordString);
+        int index = words.getLowerBound(wordString);
         if (index < 0) {
-            System.out.println("There is no that word in the dictionary!");
+            System.out.println("This word is not in the dictionary!");
         } else {
             Word word = words.getWords().get(index);
             System.out.println("Word target: " + word.getWordTarget());
             System.out.println("Word explain: " + word.getWordExplain());
         }
         System.out.println();
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\n[2] Continue to Look Up\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
+        } else if (type == 2) {
+            Lookup();
+        } else if (type == 2) {
+            Lookup();
+        }
     }
 
     public void Search() {
+        System.out.print("\033\143");
         System.out.print("Word you want to search: ");
         String wordString = sc.next();
-        System.out.println();
-        String[] result = words.search(wordString);
+        Word[] result = words.search(wordString);
         System.out.print("Search result : ");
         for (int i = 0; i < result.length; ++i) {
             if (i < result.length - 1)
-                System.out.print(result[i] + ", ");
+                System.out.print(result[i].getWordTarget() + ", ");
             else
-                System.out.print(result[i] + ".");
+                System.out.println(result[i].getWordTarget() + ".");
         }
         System.out.println();
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\n[2] Continue to Search\n[3] Look up\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
+        } else if (type == 2) {
+            Search();
+        } else if (type == 3) {
+            Lookup();
+        } else if (type == 2) {
+            Search();
+        } else if (type == 3) {
+            Lookup();
+        }
     }
 
     public void game() {
     }
 
     public void importFromFile() {
+        System.out.print("\033\143");
+        System.out.println("Please add * before a word you want to overwrite");
+        System.out.println();
         System.out.print("Name of file you want to import from: ");
         String fileName = sc.next();
+        ArrayList<String> summary = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("data/CLI/" + fileName))) {
             String line;
             ArrayList<Word> wordDuplicate = new ArrayList<>();
@@ -138,17 +260,29 @@ public class CommandLine {
                 if (parts.length == 2) {
                     String wordTarget = parts[0];
                     String wordExplain = parts[1];
-                    Word newWord = new Word(wordTarget, wordExplain, null, null);
+                    boolean overwrite = false;
+                    if(wordTarget.charAt(0) == '*') {
+                        wordTarget = wordTarget.substring(1);
+                        overwrite = true;
+                    }
+                    Word newWord = new Word(wordTarget, wordExplain);
 
-                    int index = words.getWordTarget().getLowerBound(wordTarget);
-                    if (index < 0) {
-                        words.insert(newWord);
+                    if (words.insert(newWord)) {
+                        summary.add(wordTarget);
                     } else {
-                        wordDuplicate.add(newWord);
+                        if(overwrite == false)
+                            wordDuplicate.add(newWord);
+                        else 
+                            words.fix(newWord);
+                        if(overwrite == false)
+                            wordDuplicate.add(newWord);
+                        else 
+                            words.fix(newWord);
                     }
                 }
             }
             if (wordDuplicate.size() > 0) {
+                System.out.println();
                 System.out.println(
                         "Some words have multiple meanings, you can only choose one meaning, please enter its meaning!");
                 for (int i = 0; i < wordDuplicate.size(); ++i) {
@@ -156,7 +290,7 @@ public class CommandLine {
                             .equals(wordDuplicate.get(i - 1).getWordTarget()))) {
                         System.out.println();
                         System.out.println("* Word target: " + wordDuplicate.get(i).getWordTarget());
-                        int index = words.getWordTarget().getLowerBound(wordDuplicate.get(i).getWordTarget());
+                        int index = words.getLowerBound(wordDuplicate.get(i).getWordTarget());
                         System.out.print("  Word explain: " + words.getWords().get(index).getWordExplain());
                         System.out.print(", " + wordDuplicate.get(i).getWordExplain());
                     } else {
@@ -167,8 +301,9 @@ public class CommandLine {
                         System.out.println(".");
                         System.out.print("Your choice: ");
                         String answer = sc.nextLine();
-                        if(i == 0) answer = sc.nextLine();
-                        int index = words.getWordTarget().getLowerBound(wordDuplicate.get(i).getWordTarget());
+                        if (i == 0)
+                            answer = sc.nextLine();
+                        int index = words.getLowerBound(wordDuplicate.get(i).getWordTarget());
                         words.getWords().get(index).setWordExplain(answer);
                     }
                 }
@@ -177,9 +312,29 @@ public class CommandLine {
             e.printStackTrace();
         }
         System.out.println();
+        System.out.println("Sum up the words you added!");
+        System.out.println();
+        System.out.println("No    |    English    |    Vietnamese");
+        for (int i = 0; i < summary.size(); ++i) {
+            int index = words.getLowerBound(summary.get(i));
+            Word word = words.getWords().get(index);
+            System.out.printf("%-6s", Integer.toString(i + 1));
+            System.out.printf("%-5s", "|");
+            System.out.printf("%-11s", word.getWordTarget());
+            System.out.printf("%-5s", "|");
+            System.out.println(word.getWordExplain());
+        }
+        System.out.println();
+
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
+        }
     }
 
     public void exportToFile() {
+        System.out.print("\033\143");
         System.out.print("Name of file you want to export to: ");
         String fileName = sc.next();
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("data/CLI/" + fileName))) {
@@ -192,49 +347,15 @@ public class CommandLine {
             e.printStackTrace();
         }
         System.out.println();
+        System.out.print("Press:\n[0] Exit\n[1] Show Menu\nYour choice: ");
+        int type = sc.nextInt();
+        if (type == 1) {
+            showMenu();
+        }
     }
 
     public static void main(String[] args) {
         CommandLine x = new CommandLine();
-        boolean OK = true;
-        while (OK) {
-            x.showMenu();
-            int type = sc.nextInt();
-            switch (type) {
-                case 0:
-                    OK = false;
-                    break;
-                case 1:
-                    x.Add();
-                    break;
-                case 2:
-                    x.Remove();
-                    break;
-                case 3:
-                    x.Update();
-                    break;
-                case 4:
-                    x.Display();
-                    break;
-                case 5:
-                    x.Lookup();
-                    break;
-                case 6:
-                    x.Search();
-                    break;
-                case 7:
-                    x.game();
-                    break;
-                case 8:
-                    x.importFromFile();
-                    break;
-                case 9:
-                    x.exportToFile();
-                    break;
-                default:
-                    OK = false;
-                    break;
-            }
-        }
+        x.showMenu();
     }
 }
