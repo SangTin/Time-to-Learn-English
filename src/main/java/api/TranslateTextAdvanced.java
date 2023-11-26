85db9961a5146315c5d58c1a2e93213224b987c3
ChamLa <22021125@vnu.edu.vn>
2023-11-26 19:09:02 +0700

    Translate text: add source language to each request

--- src/main/java/api/TranslateTextAdvanced.java
+++ src/main/java/api/TranslateTextAdvanced.java
@@ -1,33 +1,44 @@
 package api;
 
 // Imports the Google Cloud Translation library.
+
 import com.google.cloud.translate.v3.LocationName;
 import com.google.cloud.translate.v3.TranslateTextRequest;
 import com.google.cloud.translate.v3.TranslateTextResponse;
 
 import java.io.IOException;
 
-
 public class TranslateTextAdvanced {
 
     /**
      * Translate source text into target language.
+     *
+     * @param sourceLanguage - language that the source text is.
      * @param targetLanguage - language that the source text is translated into.
-     *         further information about supported languages: https://cloud.google.com/translate/docs/languages
-     * @param text - source text needed to be translated.
+     *                       further information about supported languages: https://cloud.google.com/translate/docs/languages
+     * @param text           - source text needed to be translated.
      * @return The text after translate to target language.
-     * */
-    public static String translateText(String targetLanguage, String text)
+     */
+    public static String translateText(String sourceLanguage, String targetLanguage, String text)
             throws IOException {
 
         //Supported location: global.
         LocationName parent = LocationName.of(CONST.PROJECT_ID, "global");
 
         //Initialize a request.
+//        TranslateTextRequest request =
+//                TranslateTextRequest.newBuilder()
+//                        .setParent(parent.toString())
+//                        .setMimeType("text/plain")
+//                        .setTargetLanguageCode(targetLanguage)
+//                        .addContents(text)
+//                        .build();
+
         TranslateTextRequest request =
                 TranslateTextRequest.newBuilder()
                         .setParent(parent.toString())
                         .setMimeType("text/plain")
+                        .setSourceLanguageCode(sourceLanguage)
                         .setTargetLanguageCode(targetLanguage)
                         .addContents(text)
                         .build();
